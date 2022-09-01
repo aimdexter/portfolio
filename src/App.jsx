187afdaba9f983c3036543ed49a1c6d347adcb84
aimdexter <draimade@gmail.com>
2022-09-01 19:30:11 +0100

    header component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
+import Header from './components/Header'
 import Menu from './components/Menu'
 import './index.css'
 
 function App() {
   return (
-    <div className="font-arial p-4">
+    <div className="font-arial p-4 flex flex-col gap-7">
       <Menu />
+      <Header />
     </div>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+const Header = () => {
+    return (
+        <div className="flex flex-col gap-7">
+            <div className="font-bold text-5xl">
+                I'm Aimade ANOUAR
+            </div>
+            <div className="text-2xl font-bold">
+                <span className="text-brand">
+                    Frontend Developer and Designer.
+                </span>
+                I learn and teach everything about Web Technologies and Product Design.
+            </div>
+        </div>
+    );
+}
+
+export default Header;
