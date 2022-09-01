2d6ee6e452fdec8b5d1902b15ad8b221a817607a
aimdexter <draimade@gmail.com>
2022-09-01 19:21:58 +0100

    menu component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
+import Menu from './components/Menu'
 import './index.css'
 
 function App() {
   return (
-    <div className="font-arial">
+    <div className="font-arial p-4">
+      <Menu />
     </div>
   )
 }
---
+++ src/components/Menu.jsx
@@ -0,0 +1,20 @@
+const Menu = () => {
+    return (
+        <div className="text-base text-brandLight flex justify-between">
+            <div className="text-brand font-bold ">
+                Home
+            </div>
+            <div className="">
+                Portfolio
+            </div>
+            <div className="">
+                About me
+            </div>
+            <div className="">
+                Contact
+            </div>
+        </div>
+    );
+}
+
+export default Menu;
