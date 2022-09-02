ee1ae06761923bb79724af5dd0e249b112f64aab
aimdexter <draimade@gmail.com>
2022-09-02 17:45:02 +0100

    project card updated

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,28 +6,28 @@ import { GrDeploy } from 'react-icons/gr';
 const Projects = () => {
     const projects = [{
         id: 1,
-        title: 'eCommerce Mobile App',
-        content: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        image: '/images/project1.svg',
+        title: 'i2i-infogerance',
+        content: 'Pwa for i2i company',
+        image: '/images/i2i.png',
         code: 'https://github.com/youssra-ghninou/i2i-infogerance',
         demo: 'https://www.info2i.com/',
     },
     {
         id: 2,
-        title: 'eCommerce Mobile App',
-        content: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        image: '/images/project1.svg',
-        code: 'https://github.com/youssra-ghninou/i2i-infogerance',
-        demo: 'https://www.info2i.com/',
+        title: 'AMAROQ',
+        content: 'Portfolio for a photographer, artist and Filmmaker',
+        image: '/images/amarok.png',
+        code: 'https://aimdexter.github.io/AMAROQ/',
+        demo: 'https://github.com/aimdexter/AMAROQ',
     },
     ];
     return (
         <div className="py-7 flex flex-col gap-5">
             {projects.map(({ id, title, content, image, code, demo }) => {
                 return (
-                    <div key={id} className="w-[358px] shadow-2xl rounded-lg">
+                    <div key={id} className="w-[358px] shadow-2xl">
                         <div className="">
-                            <img className="rounded-lg py-2" src={image} alt={title} />
+                            <img className="rounded-3xl py-2" src={image} alt={title} />
                         </div>
                         <div className="px-4 pb-4">
                             <div className="text-2xl text-brand font-bold py-2">
