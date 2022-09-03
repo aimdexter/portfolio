756621f5f033f304acd7f1e007a261dd7ad54253
aimdexter <draimade@gmail.com>
2022-09-03 02:11:09 +0100

    added dark mode toggle

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,4 +1,5 @@
 import { FaHome } from 'react-icons/fa';
+import { MdDarkMode } from 'react-icons/md';
 import { ImProfile } from 'react-icons/im';
 import { AiFillContacts, AiOutlineAppstore } from 'react-icons/ai';
 
@@ -7,12 +8,7 @@ const Menu = () => {
         <div class="">
             <section id="bottom-navigation" class="block fixed inset-x-0 lg:top-0 lg:h-fit bottom-0 z-10 bg-black text-white">
                 <div id="tabs" class="flex justify-between font-bold">
-                    <a href="#" class="flex flex-col gap-1 justify-center items-center w-full focus:text-teal-500 hover:text-teal-500 pt-2 pb-1">
-                        <div className="text-xl">
-                            <FaHome />
-                        </div>
-                        <span class="text-xs ">Home</span>
-                    </a>
+
                     <a href="#" class="flex flex-col gap-1 justify-center items-center w-full focus:text-teal-500 hover:text-teal-500 pt-2 pb-1">
                         <div className="text-xl">
                             <AiOutlineAppstore />
@@ -27,10 +23,22 @@ const Menu = () => {
                     </a>
                     <a href="#" class="flex flex-col gap-1 justify-center items-center w-full focus:text-teal-500 hover:text-teal-500 pt-2 pb-1">
                         <div className="text-xl">
+                            <FaHome />
+                        </div>
+                        <span class="text-xs ">Home</span>
+                    </a>
+                    <a href="#" class="flex flex-col gap-1 justify-center items-center w-full focus:text-teal-500 hover:text-teal-500 pt-2 pb-1">
+                        <div className="text-xl">
                             <AiFillContacts />
                         </div>
                         <span class="text-xs">Contact</span>
                     </a>
+                    <div class="flex flex-col gap-1 justify-center items-center w-full focus:text-teal-500 hover:text-teal-500 pt-2 pb-1">
+                        <div className="text-xl">
+                            <MdDarkMode />
+                        </div>
+                        <span class="text-xs">Dark Mode</span>
+                    </div>
                 </div>
             </section>
         </div>
